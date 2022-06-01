041687d5dddb5b7a70153bf3cf860fb2d7b80a55
Tikhova <tikhova.uliana@gmail.com>
2022-06-01 15:45:12 +0400

    Блокировка кнопки, пока колесо не докрутится

--- Assets/Scripts/UI/StartButtonView.cs
+++ Assets/Scripts/UI/StartButtonView.cs
@@ -8,13 +8,19 @@ public class StartButtonView : FortuneWheelElement
 {
     private void Awake()
     {
-        var button = GetComponent<Button>();
-        button.onClick.AddListener(LaunchWheel);
+        GetComponent<Button>().onClick.AddListener(LaunchWheel);
     }
 
     private void LaunchWheel()
     {
+        GetComponent<Button>().enabled = false;
         Sequence rotationSequence = DOTween.Sequence();
         Game.Controller.RotationController.CollectSequence(rotationSequence);
+        rotationSequence.OnComplete(ActivateButton);
+    }
+
+    public void ActivateButton()
+    {
+        GetComponent<Button>().enabled = true;
     }
 }
